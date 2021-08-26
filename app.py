09ae0eaf06329238ae0c2cf2682f505d54b13167
Rahul Ranjan Sinha <luharihsin@gmail.com>
2021-08-26 19:36:04 +0530

    Added python requirement and docker-compose

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'db',
+  'port': '3306',
+  'database': 'feedback',
+  'raise_on_warnings': True
+}
+
+# cnx = mysql.connector.connect(**config)
+
+# cnx.close()
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
